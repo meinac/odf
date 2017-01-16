9109cad05e74388edeaf1e56d94e78037f4003ea
Mehmet Emin INAC <mehmetemininac@gmail.com>
2017-01-16 12:05:28 +0100

    Delegate `add_child` to the root of parent

--- lib/odf/element/abstract.rb
+++ lib/odf/element/abstract.rb
@@ -1,15 +1,19 @@
+# Abstract elements do not contain content,
+# they contain only other elements.
 module Odf::Element
   class Abstract
 
+    # +root+ stands for the root xml element.
+    # +parent+ stands for the parent odf element.
     attr_reader :parent, :root
 
     def initialize(parent, options = {})
       @parent = parent
       @root   = document.content.xml.create_element(xml_tag, fetch_options(options))
-      @parent.root.add_child(@root)
+      @parent.add_child(@root)
     end
 
-    # Accessor for valid options of element
+    # Accessor for valid options of element.
     def valid_options
       @valid_options ||= defined?(self.class::VALID_OPTIONS) ? self.class::VALID_OPTIONS : {}
     end
@@ -18,8 +22,15 @@ module Odf::Element
       self.class::XML_TAG
     end
 
+    # Delegates the `document` method of the parent.
+    # This document object is crucial for creating xml elements.
     def document
       parent.document
+    end
+
+    # Delegates the `add_child` method of the root.
+    def add_child(element)
+      root.add_child(element)
     end
 
     private
--- lib/odf/element/abstract_contenteable.rb
+++ lib/odf/element/abstract_contenteable.rb
@@ -1,10 +1,13 @@
+# Contenteable elements contain content,
+# e.g. table cell contains numbers.
 module Odf::Element
   module AbstractContenteable
 
+    # Override the Odf::Element::Abstract to accept content.
     def initialize(parent, content, options = {})
       @parent = parent
       @root   = document.content.xml.create_element(xml_tag, content, fetch_options(options))
-      @parent.root.add_child(@root)
+      @parent.add_child(@root)
     end
 
   end
